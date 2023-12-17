a089b44c2723cd5e588bafffb9460ee1bce7d2b7
Sean A. Irvine <sairvin@gmail.com>
2023-12-18 07:40:50 +1300

    Support knight moves in wordsearch

--- src/irvine/language/WordSearch.java
+++ src/irvine/language/WordSearch.java
@@ -18,10 +18,13 @@ public final class WordSearch extends Command {
 
   private static final String MIN_LENGTH_FLAG = "min-length";
   private static final String FREE_FLAG = "free";
+  private static final String KNIGHT_FLAG = "knight";
 
   private static final int[] DELTA_X = {1, -1, 0, 0, 1, -1, 1, -1};
   private static final int[] DELTA_Y = {0, 0, 1, -1, 1, 1, -1, -1};
   private static final String[] DIR = {"E", "W", "S", "N", "SE", "SW", "NE", "NW"};
+  private static final int[] KNIGHT_DELTA_X = {2, -2, 2, -2, 1, -1, 1, -1};
+  private static final int[] KNIGHT_DELTA_Y = {1, 1, -1, -1, 2, 2, -2, -2};
 
   /** Construct the module. */
   public WordSearch() {
@@ -63,6 +66,25 @@ public final class WordSearch extends Command {
     }
   }
 
+  // Search with freedom and movement of a chess knight (apart from no reuse of positions)
+  private void searchKnight(final PrintStream out, final int sx, final int sy, final int x, final int y, final Trie child, final String soFar) {
+    if (child == null) {
+      return; // No match
+    }
+    if (child.isTerminal()) {
+      out.println(soFar.toUpperCase(Locale.getDefault()) + " (" + sx + "," + sy + ")");
+    }
+    for (int d = 0; d < KNIGHT_DELTA_X.length; ++d) {
+      final int nx = x + KNIGHT_DELTA_X[d];
+      final int ny = y + KNIGHT_DELTA_Y[d];
+      if (ny >= 0 && ny < mGrid.length && nx >= 0 && nx < mGrid[ny].length && !mUsed[ny][nx]) {
+        mUsed[ny][nx] = true;
+        searchKnight(out, sx, sy, nx, ny, child.getChild(mGrid[ny][nx]), soFar + mGrid[ny][nx]);
+        mUsed[ny][nx] = false;
+      }
+    }
+  }
+
   @Override
   public void mainExec(final String... args) {
     final CliFlags flags = new CliFlags(getDescription());
@@ -71,6 +93,7 @@ public final class WordSearch extends Command {
     CommonFlags.registerOutputFlag(flags);
     flags.registerOptional('m', MIN_LENGTH_FLAG, Integer.class, "INT", "minimum length of word", 3);
     flags.registerOptional(FREE_FLAG, "direction is allowed to change at each step");
+    flags.registerOptional(KNIGHT_FLAG, "each step follows the movement of a chess knight");
     flags.registerRequired(String.class, "STRING", "rows of the grid").setMaxCount(Integer.MAX_VALUE);
     flags.setValidator(f -> {
       if (!CommonFlags.validateDictionary(f)) {
@@ -97,6 +120,7 @@ public final class WordSearch extends Command {
     flags.setFlags(args);
 
     final boolean free = flags.isSet(FREE_FLAG);
+    final boolean knight = flags.isSet(KNIGHT_FLAG);
 
     final int minLength = (Integer) flags.getValue(MIN_LENGTH_FLAG);
     mGrid = new char[flags.getAnonymousValues(0).size()][];
@@ -110,12 +134,16 @@ public final class WordSearch extends Command {
       throw new RuntimeException("Problem reading word list.", e);
     }
     try (final PrintStream out = CommonFlags.getOutput(flags)) {
-      if (free) {
+      if (free || knight) {
         mUsed = new boolean[mGrid.length][mGrid[0].length];
         for (int y = 0; y < mGrid.length; ++y) {
           for (int x = 0; x < mGrid[y].length; ++x) {
             mUsed[y][x] = true;
-            search(out, x, y, x, y, mWords.getChild(mGrid[y][x]), String.valueOf(mGrid[y][x]));
+            if (knight) {
+              searchKnight(out, x, y, x, y, mWords.getChild(mGrid[y][x]), String.valueOf(mGrid[y][x]));
+            } else {
+              search(out, x, y, x, y, mWords.getChild(mGrid[y][x]), String.valueOf(mGrid[y][x]));
+            }
             mUsed[y][x] = false;
           }
         }
